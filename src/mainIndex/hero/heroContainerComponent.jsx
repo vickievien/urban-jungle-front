aacebf9524471f03444b2fb293a10b916326024c
Vickie Vien <vickie@DESKTOP-A7CACKI.localdomain>
2022-03-30 15:07:52 -0700

    hero added

--- src/mainIndex/hero/heroContainerComponent.jsx
+++ src/mainIndex/hero/heroContainerComponent.jsx
@@ -5,8 +5,8 @@ import '../../App.css';
 const HeroContainerComponent = () => {
   return (
     <section className='hero-container'>
-        <img className='hero-pic' src="https://i.imgur.com/SMMxAFy.jpg" alt="hero plant image" />
-        <img className='hero-logo-overlay'/>
+        <img className='hero-pic' src="https://i.imgur.com/EXXgfmu.jpg" alt="hero plant image" />
+        <h1 className='hero-title'>urban <br /> jungle</h1>
     </section>
   );
 }
