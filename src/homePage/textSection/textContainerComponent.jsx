a681a1daf8e48ed6092af645af41e4e59af7c456
vickievien <vickievien@gmail.com>
2022-04-20 19:28:31 -0700

    quote section done

--- src/homePage/textSection/textContainerComponent.jsx
+++ src/homePage/textSection/textContainerComponent.jsx
@@ -5,12 +5,14 @@ import '../../css/home.css';
 const TextContainerComponent = () => {
     return(
         <section className='text-section'>
-            <p className='text-section-quote q-line-one'>I don't need less plants...</p>            
-            <p className='text-section-quote q-line-two'>I need more shelves.</p>
-            <p className='text-section-quote q-line-three'>- Anonymous</p>
-            <div className='seed-container'>
-                <i class="fas fa-seedling seed-one"></i>
-                <i class="fas fa-seedling seed-two"></i>
+            <div className='text-seed-container'>
+                <p className='text-section-quote q-line-one'>I don't need less plants...</p>            
+                <p className='text-section-quote q-line-two'>I need more shelves.</p>
+                <p className='text-section-quote q-line-three'>- Anonymous</p>            
+                <div className='seed-container'>
+                    <i class="fas fa-seedling seed-one"></i>
+                    <i class="fas fa-seedling seed-two"></i>
+                </div>
             </div>
         </section>
     )
